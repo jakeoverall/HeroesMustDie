fd8373d13bc54b0ceb9b59b0ad4922345e11f2d3
Jake Overall <joverall22@gmail.com>
2014-11-14 21:35:06 -0700

    add boss and hero

---
+++ Assets/boss/scripts/Boss.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss : MonoBehaviour {
+
+	public string Name = "Why you no Name me :(";
+	public int Health = 500;
+	public int Charisma = 100;
+	public int Gold = 500;
+
+	//public Boss daBoss = new Boss ("Billy", 500, 100, 500);
+	// Use this for initialization
+	void Start () {
+		Rigidbody2D daBoss = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
